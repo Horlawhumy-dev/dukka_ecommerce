704f871240c4afc3aa43aaa925af52f536f3765d
Arafat Olayiwola <harof.dev@gmail.com>
2023-04-27 18:16:04 +0100

    add auth test cases

--- dukka_ecommerce/dukka_ecommerce/urls.py
+++ dukka_ecommerce/dukka_ecommerce/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+app_name = "dukka_ecommerce"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
